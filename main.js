c44ff25406a139d7968dac88b99a9c4aeb5d360e
LFF5644 (Code-Server) <meerschweinchen5644@gmail.com>
2023-03-02 20:56:52 +0100

    uhrzeit mittig!

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const importFolder="/home/lff/bind/myOwnProgrammes/nodejs/require";	// https://g
 const cursor=require(importFolder+"/cursor.js");
 const figlet=require("figlet");
 const {spawn}=require("child_process");
+const {LINES,COLUMNS}=process.env;
 
 function getTime(){
 	const date=new Date();
@@ -18,11 +19,26 @@ const spawnedProcess=spawn("cmatrix",["-u","10"]);
 spawnedProcess.stdout.on("data",buffer=>{
 	let text=buffer.toString("utf-8");
 
+	const time=getTime();
+	const showColumn=(COLUMNS/2)-time.split("\n")[0].length/2;
+	const showLine=(LINES/2)-time.split("\n").length/2;
+
 	text+=cursor.changePosition(1,1);
 	text+="\x1b[0m";	// color reset
 	text+="\x1b[31m";	// color to red
 	text+="\x1b[1m";	// color to bright
-	text+=getTime();
+	text+=(time
+		.split("\n")
+		.map((item,index)=>(
+			cursor.changePosition(
+					showLine+index,
+					showColumn
+				)+
+				item
+			)
+		)
+		.join("\n")
+	);
 	text+="\x1b[0m";	// color reset
 
 	process.stdout.write(text);
