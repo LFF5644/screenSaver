a7266a0a4953561bc04fa982acf6379276efe91d
LFF5644 (Code-Server) <meerschweinchen5644@gmail.com>
2023-03-02 20:35:29 +0100

    bettercode

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 #!/bin/env node
+const importFolder="/home/lff/bind/myOwnProgrammes/nodejs/require";	// https://github.com/LFF5644/node-modules
+const cursor=require(importFolder+"/cursor.js");
 const figlet=require("figlet");
 const {spawn}=require("child_process");
-
-const data={
-	lastTime:null,
-}
 
 function getTime(){
 	const date=new Date();
@@ -12,31 +10,20 @@ function getTime(){
 	const minutes=String(date.getMinutes()).padStart(2,"0");
 	const logStr=`${hours.split("").join(" ")} : ${minutes.split("").join(" ")}`
 	const text=figlet.textSync(logStr);
-	const res={
-		changed:true,
-		text,
-	};
-
-	if(data.lastTime==text){
-		res.changed=false;
-	}else{
-		data.lastTime=text;
-	}
-
-	return res;
+	return text;
 }
 
-const spawnedProcess=spawn("cmatrix",["-u","10"],{
-	env:process.env,
-});
+const spawnedProcess=spawn("cmatrix",["-u","10"]);
 
 spawnedProcess.stdout.on("data",buffer=>{
 	let text=buffer.toString("utf-8");
 
-	text+=savePos();
-	text+=changeCursorPos(1,1);
-	text+=getTime().text;
-	text+=restorePos();
+	text+=cursor.changePosition(1,1);
+	text+="\x1b[0m";	// color reset
+	text+="\x1b[31m";	// color to red
+	text+="\x1b[1m";	// color to bright
+	text+=getTime();
+	text+="\x1b[0m";	// color reset
 
 	process.stdout.write(text);
 });
