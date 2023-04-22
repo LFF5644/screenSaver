95895fe063279577856b53e5612bbdcb3f81fcdd
LFF5644 (Code-Server) <meerschweinchen5644@gmail.com>
2023-04-22 13:40:01 +0200

    bug fix

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const cursor=require(importFolder+"/cursor.js");
 const figlet=require("figlet");
 const {spawn}=require("child_process");
 const {LINES,COLUMNS}=process.env;
+let message="";
 
 function getTime(){
 	const date=new Date();
@@ -15,20 +16,29 @@ function getTime(){
 }
 
 const spawnedProcess=spawn("cmatrix",["-u","10"]);
-
 spawnedProcess.stdout.on("data",buffer=>{
-	let text=buffer.toString("utf-8");
+	let text="";
+	text+="\033[u";		// load cursor position
+	text+=buffer.toString("utf-8");
+	text+="\033[s"; 	// save cursor position
 
 	const time=getTime();
-	const showColumn=(COLUMNS/2)-time.split("\n")[0].length/2;
-	const showLine=(LINES/2)-time.split("\n").length/2;
+	const showColumn=Math.round((COLUMNS/2)-time.split("\n")[0].length/2);
+	const showLine=Math.round((LINES/2)-time.split("\n").length/2);
 
 	text+=cursor.changePosition(1,1);
 	text+="\x1b[0m";	// color reset
-	text+="\x1b[31m";	// color to red
+	text+="\x1b[34m";	// color to blue
 	text+="\x1b[1m";	// color to bright
 	text+=(time
 		.split("\n")
+		.map(item=>item
+			.split("")
+			.map(i=>i!==" "?i:(
+				"\033[1C"	// move cursor "->"
+			))
+			.join("")
+		)
 		.map((item,index)=>(
 			cursor.changePosition(
 					showLine+index,
@@ -37,9 +47,27 @@ spawnedProcess.stdout.on("data",buffer=>{
 				item
 			)
 		)
-		.join("\n")
+		.join("\n")+"\n"+message
 	);
 	text+="\x1b[0m";	// color reset
 
 	process.stdout.write(text);
+});
+
+process.stdin.setRawMode(true);
+process.stdin.on("data",buffer=>{
+	const char=buffer.toString("utf-8");
+	if(char==="q"){
+		process.stdout.write(
+			cursor.changePosition(1,1)+
+			"\x1b[0m"+	// color reset
+			"\033[2J"	// clear console
+		);
+		process.exit(0);
+	}
+	if(char==="\r"){
+		message="";
+		return;
+	}
+	message+=buffer.toString("utf-8");
 });
