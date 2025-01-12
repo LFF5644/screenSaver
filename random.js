ff96effe7000edc6f2294af4cf993934d3715764
LFF5644 <meerschweinchen5644@gmail.com>
2025-01-12 15:47:32 +0100

    nowCircelsButSlow

--- random.js
+++ random.js
@@ -18,47 +18,73 @@ function randomCords(offset=0){
 	return [x,y];
 }
 
+const circles=[];
+
 function createStar(){
 	const starColor=[255,255,255];
-	const [startX,startY]=randomCords(offset=50);
+	const [x,y]=randomCords(50);
 
-	const rectangleWidth2=randomNumber(10,5);
-	const rectangleWidth=rectangleWidth2*2;
+	const radius=randomNumber(50,0);
 
-	{ //main
-		const x=startX-rectangleWidth2
-		const y=startY-rectangleWidth2
-		fb.writeRectangle(x,y,rectangleWidth,rectangleWidth,...starColor);
-	}
-
-	{ //bottom
-		const x=startX-Math.round(rectangleWidth2/2);
-		const y=startY+rectangleWidth2
-		fb.writeRectangle(x,y,rectangleWidth2,rectangleWidth2,...starColor);
-	}
-	{ //top
-		const x=startX-Math.floor(rectangleWidth2/2);
-		const y=startY-rectangleWidth;
-		fb.writeRectangle(x,y,rectangleWidth2,rectangleWidth2,...starColor);
-	}
-	{ //left
-		const x=startX-rectangleWidth+1;
-		const y=startY-Math.round(rectangleWidth2/2);
-		fb.writeRectangle(x,y,rectangleWidth2,rectangleWidth2,...starColor);
-	}
-	{ //right
-		const x=startX+rectangleWidth2;
-		const y=startY-Math.round(rectangleWidth2/2);
-		fb.writeRectangle(x,y,rectangleWidth2,rectangleWidth2,...starColor);
-	}
+	if(checkCirclesTouchCircle(x,y,radius)) return;
+	drawCircle(x,y,radius,255,255,255);
+	circles.push([x,y,radius]);
 
 
 	//fb.writePixel(startX,startY,255,255,255);
 }
 
+function checkCirclesTouchCircle(centerX,centerY,radius){
+	for(const circle of circles){
+		const [circleX,circleY,circleRadius]=circle;
+		for(let y=circleY-circleRadius; y<=circleY+circleRadius; y+=1){
+			for(let x=circleX-circleRadius; x<=circleX+circleRadius; x+=1){
+				
+				for(let newY=centerY-radius; newY<=centerY+radius; newY+=1){
+					for(let newX=centerX-radius; newX<=centerX+radius; newX+=1){
+						
+						const dx=x-centerX;
+						const dy=y-centerY;
+						const circle_dx=newX-circleX;
+						const circle_dy=newY-circleY;
+
+						if(Math.sqrt(dx*dx+dy*dy)<=radius){
+							return true;
+						}
+					}
+				}
+			}
+		}
+	}
+	return false;
+}
+function drawCircle(centerX,centerY,radius,...color) {
+	for(let y=centerY-radius; y<=centerY+radius; y+=1) {
+		for(let x=centerX-radius; x<=centerX+radius; x+=1) {
+			// Berechne den Abstand des Pixels vom Kreismittelpunkt
+			const dx=x-centerX;
+			const dy=y-centerY;
+
+			// Überprüfe, ob der Punkt innerhalb des Kreises liegt
+			if(Math.sqrt(dx*dx+dy*dy)<(Math.round(radius/1.5))){
+				fb.writePixel(x,y,...color); // Pixel setzen
+			}
+			else if(Math.sqrt(dx*dx+dy*dy)<=radius){
+				fb.writePixel(x,y,255,0,0);
+			}
+		}
+	}
+}
+
+fb.writeFrame();
+
 fb.clearScreen(...bgColor);
 console.log("writing stars...");
-for(let i=0; i<1e2; i+=1){
-	createStar();
-}
-fb.writeFrame();
+(async ()=>{
+	for(let i=0; i<50; i+=1){
+		createStar();
+		await fb.writeFrame();
+		fb.writeFrame();
+	}
+})();
+
